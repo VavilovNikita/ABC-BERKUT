9dcfb2135db0165f4f58c21145af83d2ba2f55f0
VavilovNikita <112397078+VavilovNikita@users.noreply.github.com>
2023-09-16 23:20:48 +0500

    initial commit

--- src/main/resources/db/changelog/sql/createBot.sql
+++ src/main/resources/db/changelog/sql/createBot.sql
@@ -3,7 +3,7 @@ CREATE TABLE bot
     id         BIGSERIAL NOT NULL PRIMARY KEY,
     name       text      NOT NULL,
     token      text      NOT NULL,
-    home_chat_id INTEGER   not null
+    home_chat_id BIGSERIAL   not null
 );
-INSERT INTO bot(id, name, token, home_chat_id)
-VALUES (1, 'VizDiz_Bot', '6466834645:AAEsCDZWRzCPsw9q-kxXoXL8lbxS2Q2KffM', '-4038952389');
+INSERT INTO bot(name, token, home_chat_id)
+VALUES ('VizDiz_Bot', '6466834645:AAEsCDZWRzCPsw9q-kxXoXL8lbxS2Q2KffM', '-4038952389');
