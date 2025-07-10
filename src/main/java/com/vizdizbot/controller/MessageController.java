afe7dcacffe17812e74908e368c28f32cb3e119a
VavilovNikita <vavilovnik00@mail.ru>
2025-07-10 11:29:37 +0300

    commit

--- src/main/java/com/vizdizbot/controller/MessageController.java
+++ src/main/java/com/vizdizbot/controller/MessageController.java
@@ -1,18 +1,16 @@
 package com.vizdizbot.controller;
 
 import com.vizdizbot.entity.Message;
-import com.vizdizbot.entity.Users;
 import com.vizdizbot.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/messages")
@@ -22,16 +20,17 @@ public class MessageController {
     private final MessageService messageService;
 
     @GetMapping("/send")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<?> sendMessage(@RequestBody Message request, Principal principal) {
         String username = principal.getName();
-        messageService.sendMessage(username, request.getMessage());
         return messageService.sendMessage(username, request.getMessage());
     }
 
-
     @GetMapping("/all")
-    public ResponseEntity<List<Message>> getMessages(@AuthenticationPrincipal Users userDetails) {
-        return messageService.getMessages(userDetails.getUsername());
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> getMessages(Principal principal) {
+        String username = principal.getName();
+        return messageService.getMessages(username);
     }
 }
 
